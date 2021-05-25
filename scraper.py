817f2283cf4e8b3e5ef9b287e8213e833b08c1db
ZacHooper <zac.g.hooper@gmail.com>
2021-05-25 12:14:07 +1000

    Inital Commit

---
+++ scraper.py
@@ -0,0 +1,8 @@
+import requests
+import pytest
+
+def example():
+    return "Hello, World"
+
+if __name__ == "__main__":
+    pass
---
+++ test_scraper.py
@@ -0,0 +1,4 @@
+import scraper
+
+def test_example():
+    assert scraper.example() == "Hello, World"
