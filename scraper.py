ce52b7fe1a361b387931b66c8191baf0a5425d09
ZacHooper <zac.g.hooper@gmail.com>
2021-05-25 13:39:57 +1000

    scraper created

--- scraper.py
+++ scraper.py
@@ -1,8 +1,114 @@
 import requests
 import pytest
+import json
+import glob
+import re
+import os
+import threading
 
-def example():
-    return "Hello, World"
+def read_json(filepath):
+    """Generic function to read return a file in json format
+
+    Args:
+        filepath (bytestram): Any file type that can be parsed into valid JSON
+
+    Returns:
+        dict: Python Dictionary -- essentially JSON
+    """
+    with open(filepath, "r") as infile:
+        return json.load(infile)
+
+def open_files():
+    """Open any files contained in the data folder
+
+    Returns:
+        list: A list of dict objects
+    """
+    filepath = "./data/*.boxdicom"
+    return [read_json(file) for file in glob.glob(filepath)]
+
+def get_all_fileVersionId(data):
+    """Looks through the provided dictionary and returns the fileVersionId for each DICOM image described
+
+    Args:
+        data (dict): The dictionary object of the study
+
+    Returns:
+        list: All the fileVersionIds of the study
+    """
+    return [o['meta']['fileVersionId'] for s in data['study']['series'] for o in s['objects']]
+
+def get_shared_name(data):
+    """Get's the shared name value from the folderUrl in the metadata
+
+    Args:
+        data (dict): dictionary of object of the studies JSON
+
+    Returns:
+        str: The shared name value
+    """
+    folderUrl = data['study']['meta']['folderUrl']
+    regex = re.compile(r'https://cloud.box.com/s/(.*)', re.I)
+    shared_name = regex.search(folderUrl)
+    return shared_name[1]
+
+def get_study_uid(data):
+    return data['study']['dicomMetadata']['StudyInstanceUID']
+
+def build_url(key, shared_name):
+    """Create the URl to be used to download the image
+
+    Args:
+        key (str): The fileVersionId
+        shared_name (str): The name of the folder the images are containined within
+
+    Returns:
+        str: The URL to use to request the imaging
+    """
+    return f"https://cloud.app.box.com/representation/file_version_{key}/dicom.dcm?shared_name={shared_name}"
+
+def build_urls(data):
+    """Returns a list of all the URLS you need to download the full study
+
+    Args:
+        data (dict): The study's JSON data
+
+    Returns:
+        list: A list of URLS required to download the full study
+    """
+    shared_name = get_shared_name(data)
+    keys = get_all_fileVersionId(data)
+    urls = [(build_url(key, shared_name), key) for key in keys]
+    return urls
+
+def download_file(folder_name, url, key):
+    print("Downloading: " + key + " with URL: " + url)
+    # Make request
+    r = requests.get(url)
+    # Save data
+    with open(f"./download_data/{folder_name}/{key}.dcm", 'wb') as f:
+        f.write(r.content)
+
+def download_files(urls, folder_name = "download_study"):
+    # Create the folder path
+    try:
+        os.mkdir('./download_data/' + folder_name)
+    except FileExistsError:
+        # If it already exists notify user and kill function    
+        print("A folder with that study uid already exists. It is likely that you have already downloaded this study")
+        return 
+
+    threads = []
+    for url, key in urls:
+        x = threading.Thread(target=download_file, args=(folder_name, url, key))
+        threads.append(x)
+        x.start()
+    
+    for thread in threads:
+        thread.join()
+
 
 if __name__ == "__main__":
-    pass
+    files = open_files()
+    for file in files:
+        download_files(build_urls(file), get_study_uid(file))
--- test_scraper.py
+++ test_scraper.py
@@ -1,4 +1,45 @@
+import json
+import pytest
 import scraper
 
-def test_example():
-    assert scraper.example() == "Hello, World"
+# Test Reading the JSON Files
+
+def test_read_json_data():
+    assert isinstance(scraper.read_json("./data/Study (1).boxdicom"), dict)
+
+def test_open_files():
+    assert isinstance(scraper.open_files(), list)
+
+def test_open_files_is_all_dicts():
+    assert all(isinstance(file, dict) for file in scraper.open_files())
+
+
+# Test parsing the JSON files 
+@pytest.fixture
+def data():
+    return scraper.read_json("./data/Study (1).boxdicom")
+
+def test_get_all_fileVersionId(data):
+    assert isinstance(scraper.get_all_fileVersionId(data), list)
+
+def test_get_shared_name(data):
+    assert scraper.get_shared_name(data) == "pi4rsya9zqphh9o7iejsrt650cl7y0hf"
+
+# Test Building Request URL
+@pytest.fixture
+def key():
+    return '73106283297'
+
+@pytest.fixture
+def shared_name():
+    return 'pi4rsya9zqphh9o7iejsrt650cl7y0hf'
+
+def test_build_url(key, shared_name):
+    url = scraper.build_url(key, shared_name)
+    assert url == f"https://cloud.app.box.com/representation/file_version_{key}/dicom.dcm?shared_name={shared_name}"
+
+def test_build_urls(data):
+    assert isinstance(scraper.build_urls(data), list)
+
+def test_build_urls_data(data):
+    assert all(isinstance(x, tuple) for x in scraper.build_urls(data))
